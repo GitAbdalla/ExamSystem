ddb20fe776564c2131e4034b8fb3fa673ffcac92
Abdalla <90405765+GitAbdalla@users.noreply.github.com>
2025-05-04 17:34:53 +0300

    userModel

--- Backend/index.js
+++ Backend/index.js
@@ -1,7 +1,9 @@
 const express = require('express')
+const dotenv = require('dotenv')
 const app = express()
 const port = 3000
 
+dotenv.config()
 app.get('/', (req, res) => {
   res.send(' branch &&بسم الله توكلنا على الله')
 })
---
+++ Backend/models/userModel.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: ["admin", "student"],
+      default: "student",
+    },
+    department: {
+      type: String,
+      enum: ["mearn", "dotnet", "python"],
+      required: () => {
+        return (this.role = "student");
+      },
+    },
+  },
+  { timestamps: true }
+);
+module.exports = mongoose.model('User', userSchema)
