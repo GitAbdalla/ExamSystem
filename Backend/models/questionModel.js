0b6d5db407001c990a09aff95acff86b09821706
Abdalla <90405765+GitAbdalla@users.noreply.github.com>
2025-05-04 18:16:43 +0300

    question model

---
+++ Backend/models/questionModel.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const questionSchema = new mongoose.Schema({
+  examId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Exam",
+    required: true,
+  },
+  questionText: {
+    type: String,
+    required: true,
+  },
+  options: [
+    {
+      type: String,
+      required: true,
+    },
+  ],
+  correctAnswer: {
+    type: String,
+    required: true,
+  },
+});
+
+export default mongoose.model("Question", questionSchema);
